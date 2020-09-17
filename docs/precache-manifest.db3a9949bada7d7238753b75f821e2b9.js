1ef268f66dd75a60151632b44646e3b32619435c
JhormaMalave <jhormamalave99@gmail.com>
2020-09-16 22:14:52 -0400

    Docs folder

---
+++ docs/precache-manifest.db3a9949bada7d7238753b75f821e2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04666cd4ef162e03321bc51444254f97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d315eb481df656ccc6e",
+    "url": "/static/css/main.52989e8e.chunk.css"
+  },
+  {
+    "revision": "7df93c7276edf1b25086",
+    "url": "/static/js/2.65d56cdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65d56cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d315eb481df656ccc6e",
+    "url": "/static/js/main.9c247826.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
